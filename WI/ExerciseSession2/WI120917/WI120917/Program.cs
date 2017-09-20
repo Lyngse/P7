c28f3d5e3cf608ccbddf97254c5fb8fbd5cfe008
smed95 <9049518+smed95@users.noreply.github.com>
2017-09-20 13:31:17 +0200

    Work on third WI session
    Add tokanizer and indexer

--- WI/ExerciseSession2/WI120917/WI120917/Program.cs
+++ WI/ExerciseSession2/WI120917/WI120917/Program.cs
@@ -4,18 +4,118 @@ using System.Collections.Generic;
 using System.Net;
 using System.Threading;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using HtmlAgilityPack;
+using SF.Snowball;
+using SF.Snowball.Ext;
 
 namespace WI120917
 {
     class Program
     {
+
+        static Dictionary<string, Robots> robotTxts = new Dictionary<string, Robots>();
+
+        static Dictionary<Uri, string> pages = new Dictionary<Uri, string>();
+
+        static Dictionary<string, Dictionary<int, List<int>>> index = new Dictionary<string, Dictionary<int, List<int>>>();
+
         static void Main(string[] args)
         {
             Uri url = new Uri("http://www.wikipedia.org");
-            crawl(url);
+
+
+            //crawl(url);
+            Indexing(); 
 
             Console.Read();
 
+        }
+
+        static void Indexing()
+        {
+            List<string> paths = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + @"\docs\").ToList();
+            List<string> stopWords = File.ReadAllText(@"StopWords.txt").Split("\r\n").ToList();
+
+            foreach (var path in paths)
+            {
+                int fileID = int.Parse(path.Split("doc").Last().Replace(".html", ""));
+                List<string> tokens = Tokenizer(File.ReadAllText(path));
+                int tokenPosition = 0;
+
+                foreach (var stopWord in stopWords)
+                {
+                    tokens.RemoveAll(x => x.Equals(stopWord));
+
+                }
+
+                var stemmer = new EnglishStemmer();
+                for (int i = 0; i < tokens.Count; i++)
+                {
+                    stemmer.SetCurrent(tokens[i]);
+                    if (stemmer.Stem())
+                    {
+                        tokens[i] = stemmer.GetCurrent();
+                    }
+                }
+
+                foreach (var token in tokens)
+                {
+                    if (index.ContainsKey(token))
+                    {
+                        if (index[token].ContainsKey(fileID))
+                        {
+                            index[token][fileID].Add(tokenPosition);
+
+                        }
+
+                        else
+                        {
+                            var newIndex = new List<int>();
+                            newIndex.Add(tokenPosition);
+                            index[token].Add(fileID, newIndex);
+                        }
+                    }
+
+                    else
+                    {
+                        var newIndex = new List<int>();
+                        newIndex.Add(tokenPosition);
+                        var newDictionary = new Dictionary<int, List<int>>();
+                        newDictionary.Add(fileID,newIndex);
+                        index.Add(token, newDictionary);
+                    }
+                    
+                    tokenPosition++;
+                }
+
+            }
+
+        }
+
+
+
+        static List<string> Tokenizer(string htmlPage)
+        {
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(htmlPage);
+            string cleanText = htmlDocument.DocumentNode.SelectSingleNode("/html/body").InnerText;
+
+            cleanText = HttpUtility.HtmlDecode(cleanText);
+
+            string tagCleanedText = Regex.Replace(cleanText, @"<[^>]*>", "");
+            tagCleanedText = tagCleanedText.Replace("-", " ").Replace("_", " ");
+
+            /*tagCleanedText = tagCleanedText.Replace("\n", " ").Replace("\r", " ").Replace("\t", " ").Replace(":", "")
+                .Replace("'", "").Replace(".", "").Replace(",", "").Replace(";", "").Replace("-", " ").Replace("_", " ").Replace("/","");*/
+            tagCleanedText = Regex.Replace(tagCleanedText, @"[^a-z|A-Z|\s]", "");
+            tagCleanedText = Regex.Replace(tagCleanedText, @"[0-9]", "");
+            tagCleanedText = Regex.Replace(tagCleanedText, @"\s+", " ");
+            tagCleanedText = tagCleanedText.ToLower();
+
+            return tagCleanedText.Split(" ").ToList();
         }
 
         static void crawl(Uri urlSeed)
@@ -27,7 +127,7 @@ namespace WI120917
 
 
             int fileNameNumber = 1;
-            while(pages.Count < 1000 && frontier.Count > 0)
+            while (pages.Count < 1000 && frontier.Count > 0)
             {
                 Uri url = frontier.Dequeue();
                 if (pages.ContainsKey(url))
@@ -37,7 +137,7 @@ namespace WI120917
                 {
                     baseUrl = url.Host;
                 }
-                catch(Exception e)
+                catch (Exception e)
                 {
                     Console.WriteLine("Bad url: {0}", url);
                     continue;
@@ -49,7 +149,7 @@ namespace WI120917
                     string directory = AppDomain.CurrentDomain.BaseDirectory + @"\docs\doc" + fileNameNumber + ".html";
                     webClient.DownloadFile(url, directory);
                 }
-                catch(Exception e)
+                catch (Exception e)
                 {
                     Console.WriteLine("Exception: " + e);
                     continue;
@@ -63,16 +163,16 @@ namespace WI120917
 
                     if (!aTag.Contains("href="))
                         continue;
-                    
+
 
                     string[] hrefAndAfter = aTag.Split("href=\"");
                     string href = "";
-                    if(hrefAndAfter.Length > 1)
+                    if (hrefAndAfter.Length > 1)
                         href = hrefAndAfter[1].Split("\"")[0];
                     else
                     {
                         hrefAndAfter = aTag.Split("href='");
-                        if(hrefAndAfter.Length > 1)
+                        if (hrefAndAfter.Length > 1)
                             href = hrefAndAfter[1].Split("'")[0];
                     }
                     string extractedUri = "";
@@ -90,7 +190,7 @@ namespace WI120917
                         continue;
                     }
                     Robots robot;
-                    if(!robotTxts.TryGetValue(baseUrl, out robot))
+                    if (!robotTxts.TryGetValue(baseUrl, out robot))
                     {
                         robot = new Robots("http://" + baseUrl + "/robots.txt");
                     }
@@ -98,9 +198,9 @@ namespace WI120917
                     if (!robot.IsPathAllowed("sw701crawlftwplz", extractedUri))
                         continue;
 
-                    if (!frontier.Contains( new Uri(extractedUri)))
+                    if (!frontier.Contains(new Uri(extractedUri)))
                     {
-                        if(extractedUri.Contains("en.wikipedia"))
+                        if (extractedUri.Contains("en.wikipedia"))
                             frontier.Enqueue(new Uri(extractedUri));
                     }
                 }
@@ -124,7 +224,7 @@ namespace WI120917
 
             var robotTxtLines = robotTxt.Split('\n');
 
-            
+
             int i = 0;
             while (!robotTxtLines[i].ToLower().StartsWith("user-agent:") || robotTxtLines[i].Split(' ')[1] != "*") {
                 if (i == robotTxtLines.Length - 1)
@@ -132,7 +232,7 @@ namespace WI120917
                     robotTxts.Add(baseUrl, result);
                     return result;
                 }
-                    
+
                 i++;
             }
             i++;
@@ -156,9 +256,5 @@ namespace WI120917
             robotTxts.Add(baseUrl, result);
             return result;
         }*/
-
-        static Dictionary<string, Robots> robotTxts = new Dictionary<string, Robots>();
-
-        static Dictionary<Uri, string> pages = new Dictionary<Uri, string>();
     }
 }
