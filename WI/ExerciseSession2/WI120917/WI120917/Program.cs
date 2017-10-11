3e6c8f62d9eea28a3c68e7060e411850e5ead034
Lyngse <superlyng@msn.com>
2017-10-11 12:34:48 +0200

    wut

--- WI/ExerciseSession2/WI120917/WI120917/Program.cs
+++ WI/ExerciseSession2/WI120917/WI120917/Program.cs
@@ -34,10 +34,9 @@ namespace WI120917
             //Add crawlerLocation for seperate crawlerfile
             if (!File.Exists(indexLocation))
             {
-                Console.WriteLine("Starting crawling for pages..");
+                Console.WriteLine("Starting indexing of local files");
                 Indexer index = new Indexer();
                 pages = crawler.Crawl();
-                Console.WriteLine("Starting indexing of files");
                 index.Tokenize(pages);
                 WriteToBinaryFile(indexLocation, pages);
 
@@ -58,8 +57,8 @@ namespace WI120917
             }
             pages.OrderByDescending(x => x.pageRank).ToList().ForEach(x => Console.WriteLine(x.Id + " - " + x.pageRank));
 
-            RankSearch ranker = new RankSearch();
-            ranker.Rank("October castle castle castle", pages);
+            //RankSearch ranker = new RankSearch();
+            //ranker.Rank("October castle castle castle", pages);
 
             Console.Read();
 
