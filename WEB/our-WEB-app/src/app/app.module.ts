6b413a9ba36f5211f7ed7a4fdb57a0195bb33cbe
Christian Dannesboe <cdanne13@student.aau.dk>
2017-10-12 11:31:39 +0200

    udvider lige min webpage ez

--- WEB/our-WEB-app/src/app/app.module.ts
+++ WEB/our-WEB-app/src/app/app.module.ts
@@ -2,13 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+import { FooterComponent } from './footer/footer.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { SearchComponent } from './mainpage/search/search.component';
+import { ShoppinglistComponent } from './mainpage/shoppinglist/shoppinglist.component';
+import { ResultpageComponent } from './resultpage/resultpage.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRouter : Routes = [
+{ path : 'mainpage', component: MainpageComponent},
+{ path : 'resultpage', component: ResultpageComponent},
+{ path : '', redirectTo: 'mainpage', pathMatch: 'full'},
+{ path: '**', redirectTo: 'mainpage', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainpageComponent,
+    SearchComponent,
+    ShoppinglistComponent,
+    ResultpageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRouter)
   ],
   providers: [],
   bootstrap: [AppComponent]
