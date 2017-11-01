c7177c12c4255393302ed469c0898c6b0e135392
Lyngse <superlyng@msn.com>
2017-11-01 13:27:41 +0100

    asd

---
+++ WI/WI2/WI2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics;
+
+namespace WI2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + @"friendships.txt";
+            string data = System.IO.File.ReadAllText(path);
+            List<User> userList = new List<User>();
+            int idCounter = 0;
+
+
+            string[] text = data.Split("\r\n");
+
+            for (int i = 0; i < text.Length; i += 5)
+            {
+                User u = new User(idCounter,
+                    text[i].Split(" ")[1],
+                    text[i + 1].TrimStart("friends: ".ToCharArray()).TrimStart().Split("\t"),
+                    text[i + 2].Split("summary: ")[1],
+                    text[i + 3].Split("review: ")[1]);
+                userList.Add(u);
+                idCounter++;
+            }
+
+            Matrix<double> diagonalMatrix;
+
+            Matrix<double> associationMatrix =  fillMatrix(userList, out diagonalMatrix);
+
+            Matrix<double> laplacianMatrix = diagonalMatrix - associationMatrix;
+            
+            
+
+            Console.WriteLine(laplacianMatrix.ToString());
+
+            Console.ReadKey();
+        }
+
+        public static Matrix<double> fillMatrix(List<User> userList, out Matrix<double> diagonalMatrix)
+        {
+            Matrix<double> associationMatrix = Matrix<double>.Build.Sparse(userList.Count, userList.Count);
+            Matrix<double> dMatrix = Matrix<double>.Build.Diagonal(userList.Count, userList.Count);
+
+            foreach (User u in userList)
+            {
+                u.genFriends(userList);
+                foreach (User friend in u.friends)
+                {
+                    associationMatrix[u.id, friend.id] = 1;                    
+                }
+                dMatrix[u.id, u.id] = u.friends.Count;
+            }
+
+            diagonalMatrix = dMatrix;
+
+            return associationMatrix;
+        }
+    }
+}
